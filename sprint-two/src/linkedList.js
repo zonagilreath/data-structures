262588cee3696a281bd7fa0c070746a72694b68f
zonagilreath <bryson.gilreath@gmail.com>
2019-05-30 10:56:55 -0500

    LinkedList implemented and passing tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    const newNode = Node(value);
+    if (list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+    }else {
+      list.tail.next = newNode;
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head){
+      const temp = list.head.value;
+      list.head = list.head.next;
+      return temp;
+    }
   };
 
   list.contains = function(target) {
+    let current = list.head;
+    while (current){
+      if (current.value !== target){
+        current = current.next;
+      }else {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
