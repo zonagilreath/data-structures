56701acce9fc7d53aebb245024e2d90547b76b55
zonagilreath <bryson.gilreath@gmail.com>
2019-05-30 19:08:44 -0500

    Set implemented and passing tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] || false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add = O(1)
+ * contains = O(1)
+ * remove = O(1)
  */
